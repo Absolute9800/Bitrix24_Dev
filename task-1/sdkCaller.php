c6a499b8b14c87b98687884c7a99f34957cc9722
Absolute9800 <p2wval@gmail.com>
2024-10-05 00:43:35 +0700

    Выполнил все необходимые вызовы.

--- task-1/sdkCaller.php
+++ task-1/sdkCaller.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 use Bitrix24\SDK\Services\ServiceBuilderFactory;
 require_once '../vendor/autoload.php';
 
-$B24 = ServiceBuilderFactory::createServiceBuilderFromWebhook(
+$B24crm = ServiceBuilderFactory::createServiceBuilderFromWebhook(
     'https://xga007.bitrix24.ru/rest/1/n2mm0p2kzfgxecbh/'
 );
 
@@ -12,19 +12,9 @@ $B24tasks = ServiceBuilderFactory::createServiceBuilderFromWebhook(
     'https://xga007.bitrix24.ru/rest/1/3idfz76z0qqhbaq2/'
 );
 
-/*
-// Получение списка задач
-$result = $B24tasks->core->call('tasks.task.list', [
-    'order' => ['ID' => 'ASC'],
-    'filter' => [],
-    'select' => ['ID', 'TITLE'],
-])->getResponseData()->getResult();
-print_r($result);
-*/
 
-/*
-// Создание контакта
-$result = $B24->core->call('crm.contact.add', [
+// Создание контакта CORE
+$result = $B24crm->core->call('crm.contact.add', [
     'fields' => [
         'NAME' => 'Глеб',
         'SECOND_NAME' => 'Егорович',
@@ -34,25 +24,61 @@ $result = $B24->core->call('crm.contact.add', [
         'ASSIGNED_BY_ID' => 1,
     ]
 ])->getResponseData()->getResult();
-print_r($result[0]);
-*/
+print_r($result);
 
-/*
-// Получение списка контактов
-$result = $B24->core->call('crm.contact.list', [
+
+
+// Создание контакта SDK
+$result = $B24crm->getCRMScope()->contact()->add([
+    'NAME' => 'Глеб',
+    'SECOND_NAME' => 'Егорович',
+    'LAST_NAME' => 'Жеглов',
+    'OPENED' => 'Y',
+    'POST' => 'Сыщик',
+    'ASSIGNED_BY_ID' => 1,
+])->getId();
+print_r($result);
+
+
+
+// Получение списка контактов CORE
+$result = $B24crm->core->call('crm.contact.list', [
     'filter' => [],
     'order' => ['ID' => 'ASC'],
     'select' => ['NAME', 'LAST_NAME'],
 ])->getResponseData()->getResult();
 print_r($result);
-*/
 
-/*
-$result = $B24->getCRMScope()->contact()->list(
+
+
+// Получение списка контактов SDK
+$result = $B24crm->getCRMScope()->contact()->list(
     $order = ['ID' => 'ASC'],
     $filter = [],
     $select = ['NAME', 'LAST_NAME'],
     $start = 1,
-)->getContacts();
+)->getCoreResponse()->getResponseData()->getResult();
 print_r($result);
-*/
+
+
+
+// Создание задачи CORE
+$result = $B24tasks->core->call('tasks.task.add', [
+    'fields' => [
+        'RESPONSIBLE_ID' => 1,
+        'TITLE' => 'REST task',
+        'DESCRIPTION' => 'Description...',
+        'DEADLINE' => '31.12.2024 12:00'
+    ]
+])->getResponseData()->getResult();
+print_r($result);
+
+
+
+// Получение списка задач core
+$result = $B24tasks->core->call('tasks.task.list', [
+    'order' => ['ID' => 'ASC'],
+    'filter' => [],
+    'select' => ['ID', 'TITLE'],
+])->getResponseData()->getResult();
+print_r($result);
